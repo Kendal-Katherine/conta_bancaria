90716002e53fa31f2758fd60282e8aaf3f22dc1c
Kendal Katherine <kendal.katherine3@gmail.com>
2023-11-23 19:08:24 -0300

    Criação de novos métodos

--- Conta_bancaria/src/conta_bancaria/Menu.java
+++ Conta_bancaria/src/conta_bancaria/Menu.java
@@ -1,5 +1,7 @@
 package conta_bancaria;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import conta_bancaria.controller.ContaController;
@@ -9,30 +11,29 @@ import conta_bancaria.model.ContaPoupança;
 import conta_bancaria.util.Cores;
 
 public class Menu {
-	
+
 	static Scanner leia = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-
 		Scanner leia = new Scanner(System.in);
-		
+
 		ContaController contas = new ContaController();
-		
+
 		int opcao, numero, agencia, tipo, aniversario;
 		String titular;
 		float saldo, limite;
-		
+
 		ContaCorrente cc1 = new ContaCorrente(contas.gerarNumero(), 123, 1, "João da Silva", 1000.00f, 100.00f);
 		contas.cadastrar(cc1);
-		
+
 		ContaPoupança cp1 = new ContaPoupança(contas.gerarNumero(), 123, 2, "Maria da Silva", 1000.00f, 12);
 		contas.cadastrar(cp1);
-		
-		while(true) {
-			
-			System.out.println(Cores.TEXT_WHITE + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT 
-					         + "****************************************************************************");
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_WHITE + Cores.ANSI_PURPLE_BACKGROUND_BRIGHT
+					+ "****************************************************************************");
 			System.out.println("                                                                            ");
 			System.out.println("                            BRANCO KKC                                      ");
 			System.out.println("                                                                            ");
@@ -50,99 +51,125 @@ public class Menu {
 			System.out.println("                                                                            ");
 			System.out.println("****************************************************************************");
 			System.out.println("Entre com a opção desejada:                                                  ");
-			System.out.println("                                                                            " + Cores.TEXT_RESET);
-			
-			opcao = leia.nextInt();
-			
-			if(opcao == 9) {
+			System.out.println(
+					"                                                                            " + Cores.TEXT_RESET);
+
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Digite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+
+			}
+			if (opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Banco KKC - Dois Ks e um C para você vencer!");
 				sobre();
 				leia.close();
 				System.exit(0);
 			}
-			
-			switch(opcao) {
-			
+
+			switch (opcao) {
+
 			case 1:
 				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
-			
+
 				System.out.println("Digite o número da Agência: ");
 				agencia = leia.nextInt();
-				
+
 				System.out.println("Digite o nome do Titular: ");
 				leia.skip("\\R");
 				titular = leia.nextLine();
-				
+
 				System.out.println("Digite o tipo da Conta (1 - CC ou 2 - CP): ");
 				tipo = leia.nextInt();
-				
+
 				System.out.println("Digite o Saldo da conta: ");
 				saldo = leia.nextFloat();
-				
-				switch(tipo) {
-					case 1 -> {
-						System.out.println("Digite o limite da conta: ");
-						limite = leia.nextFloat();
-						contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
-					}
-					case 2 -> {
-						System.out.println("Digite o dia do aniversário da conta: ");
-						aniversario = leia.nextInt();
-						contas.cadastrar(new ContaPoupança(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
-					}
+
+				switch (tipo) {
+				case 1 -> {
+					System.out.println("Digite o limite da conta: ");
+					limite = leia.nextFloat();
+					contas.cadastrar(new ContaCorrente(contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
 				}
-				
-                break;
+				case 2 -> {
+					System.out.println("Digite o dia do aniversário da conta: ");
+					aniversario = leia.nextInt();
+					contas.cadastrar(
+							new ContaPoupança(contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
+				}
+				}
+				keyPress();
+				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-					contas.listarTodas();
+				contas.listarTodas();
+				keyPress();
 				break;
 			case 3:
 				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
 
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
+
+				contas.procurarPorNumero(numero);
+				keyPress();
 				break;
 			case 4:
 				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
-
+				keyPress();
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+				
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
 
+				contas.deletar(numero);
+				keyPress();
 				break;
 			case 6:
 				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
-
+				keyPress();
 				break;
 			case 7:
 				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-
+				keyPress();
 				break;
 			case 8:
 				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
-
+				keyPress();
 				break;
 			default:
 				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				keyPress();
 				break;
-			
-			
-			
-			
-			
-			
+
 			}
-			
-			
-			
+
 		}
-		
 
 	}
+
 	public static void sobre() {
-	System.out.println("****************************************************************************");
-	System.out.println("Projeto desenvolvido por: Kendal Katherine Correia                          ");
-	System.out.println("Apoio Generation Brasil - generation@generation.org                         ");
-	System.out.println("https://github.com/Kendal-Katherine/conta_bancaria                         ");
-	System.out.println("****************************************************************************");
+		System.out.println("****************************************************************************");
+		System.out.println("Projeto desenvolvido por: Kendal Katherine Correia                          ");
+		System.out.println("Apoio Generation Brasil - generation@generation.org                         ");
+		System.out.println("https://github.com/Kendal-Katherine/conta_bancaria                         ");
+		System.out.println("****************************************************************************");
+	}
+
+	public static void keyPress() {
+		try {
+
+			System.out.println("\n\nPreesione a tecla Enter para continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla inválida!");
+
+		}
 	}
 }
